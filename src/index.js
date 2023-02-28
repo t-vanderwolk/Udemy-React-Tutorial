98e67947de27ded94c9ce61919067da41b070b9e
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-02-28 10:48:40 -0700

    BookList Project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-function Greeting () {
-    return( 
-  <div>
- <Person />
- <Message />
-  </div>
-  );
+const BookList =() => {
+  return <section>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
 }
-
-const Person = () => <h2>John Doe</h2>;
-const Message = () =>{
-return  <p>This is my message</p>;
-} 
+const Book = () => {
+  return <article>
+<Image />
+<Title />
+<Author />
+  </article>
+}
+const Image = () => <img src="https://m.media-amazon.com/images/I/41Pw77onK7L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt="The Courage to Be Free"/>
+const Title = () => <h2>The Courage to Be Free: Florida's Blueprint for America's Revival</h2>
+const Author = () => {
+  return (
+  <h4>Ron DeSantis</h4>
+  )
+}
 
 
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
  
