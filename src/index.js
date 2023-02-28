6759898ca0863d953fd5684cb3779aa2461c826d
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-02-28 10:16:35 -0700

    removed boilerplate, added extensions and settings, Create Element function and JSX rules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const Greeting = () => {
-    return <h2>My First Component</h2>;
-  }
+function Greeting () {
+    return( 
+    <h2>Hello world</h2>);
+//       <>
+//     <div className="someValue">
+//       <h3>Hello People</h3>
+//       <ul>
+//         <li>
+//           <a href="#">
+// Hello World
+//           </a>
+//         </li>
+//       </ul>
+//     </div>
+//     <h2>hello world</h2>
+//     <input type='text' name='' id='' />
+//     </>);
+  };
+
+
   const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greeting />);
