faddb583f1fb4174dff989ebbc35ac1df44f196d
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-01 11:28:14 -0700

    JSX

--- src/index.js
+++ src/index.js
@@ -4,63 +4,73 @@ import './index.css';
 
 const BookList =() => {
   return <section className="booklist">
-    <Book1 />
-    <Book2 />
-    <Book3 />
+    <Book />
+    <Book />
+    <Book />
    
   </section>
 }
-const Book1 = () => {
-  return <article className="book">
-<Image1 />
-<Title1 />
-<Author1 />
-  </article>
-}
-const Image1 = () => <img src="./images/book-1.jpg" alt="The Courage to Be Free"/>
-const Title1 = () => <h2>The Courage to Be Free: Florida's Blueprint for America's Revival</h2>
-const Author1 = () => {
-  return (
-    //inline styling option 1
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
-  Ron DeSantis</h4>
-  )
-}
-const Book2 = () => {
-  return <article className="book">
-<Image2 />
-<Title2 />
-<Author2 />
-  </article>
-}
-const Image2 = () => <img src="./images/book-2.jpg" alt="Atomic Habits"/>
-const Title2 = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>
-const Author2= () => {
-  //inline styling option 2
-const inlineHeadingStyles = {
-  color: '#617d98',
-  fontSize: '0.75rem',
-  marginTop: '0.5rem',
+const author = "Ron DeSantis";
+const Book =()=>{
+
+const title = "The Courage to Be Free: Florida's Blueprint for America's Revival";
+  return(
+    <article className="book">
+      <img
+      src="./images/book-1.jpg"
+      alt="The Courage to Be Free" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
 };
-return <h4 style={inlineHeadingStyles}>Jordan Moore </h4>;
-};
+// const Book1 = () => {
+//   return <article className="book">
+// <Image1 />
+// <Title1 />
+// <Author1 />
+//   </article>
+// }
+// const Image1 = () => <img src="./images/book-1.jpg" alt="The Courage to Be Free"/>
+// const Title1 = () => <h2>The Courage to Be Free: Florida's Blueprint for America's Revival</h2>
+// const Author1 = () => {
+//   return (
+  
+//   <h4>
+//   Ron DeSantis</h4>
+//   )
+// }
+// const Book2 = () => {
+//   return <article className="book">
+// <Image2 />
+// <Title2 />
+// <Author2 />
+//   </article>
+// }
+// const Image2 = () => <img src="./images/book-2.jpg" alt="Atomic Habits"/>
+// const Title2 = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>
+// const Author2= () => {
+
+
+// return <h4> Jordan Moore </h4>;
+// };
   
 
-const Book3 = () => {
-  return <article className="book">
-<Image3 />
-<Title3 />
-<Author3 />
-  </article>
-}
-const Image3 = () => <img src="./images/book-3.jpg" alt="The Courage to Be Free"/>
-const Title3 = () => <h2>My Little Golden Book About Betty White</h2>
-const Author3 = () => {
-  return (
-    //styled in index.css
-  <h4>Deborah Hopkinson</h4>
-  )
-}
+// const Book3 = () => {
+//   return <article className="book">
+// <Image3 />
+// <Title3 />
+// <Author3 />
+//   </article>
+// }
+// const Image3 = () => <img src="./images/book-3.jpg" alt="The Courage to Be Free"/>
+// const Title3 = () => <h2>My Little Golden Book About Betty White</h2>
+// const Author3 = () => {
+//   return (
+   
+//   <h4>Deborah Hopkinson</h4>
+//   )
+// }
 
 
 
