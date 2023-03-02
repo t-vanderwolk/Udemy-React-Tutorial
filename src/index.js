77ec75158201da678534dcbd4f5bb8d4e37021ae
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-02 12:05:02 -0700

    props multiple approaches

--- src/index.js
+++ src/index.js
@@ -3,29 +3,48 @@ import ReactDOM  from "react-dom/client";
 import './index.css';
 
 
-const author = 'Ron DeSantis';
-const title = "The Courage to Be Free: Florida's Blueprint for America's Revival";
-const img = './images/book-1.jpg';
+
+
+const Book1 ={
+  author: "Ron DeSantis",
+  title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
+  img: './images/book-1.jpg'
+}
+
+const Book2 ={
+  author: "James Clear",
+  title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  img: './images/book-2.jpg'
+}
+
+const Book3 ={
+  author: "Deborah Hopkinson",
+  title: "My Little Golden Book About Betty White",
+  img: './images/book-3.jpg'
+}
 
 const BookList =() => {
+
   return (
   <section className="booklist">
-    <Book author={author} title={title} img={img}/>
-    <Book author={author} title={title} />
+    <Book author={Book1.author} title={Book1.title} img={Book1.img}/>
+    <Book author={Book2.author} title={Book2.title} img={Book2.img} />
+    <Book author={Book3.author} title={Book3.title} img={Book3.img} />
 
   </section>
   );
 };
-const Book = (props) => {
-  console.log(props);
+const Book = ({img , title, author}) => {
+//   console.log(props);
+// const {img, title, author} = props
   return (
     <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author} </h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
     </article>
   );
-};
+ };
 // const Book1 = () => {
 //   return <article className="book">
 // <Image1 />
@@ -54,7 +73,7 @@ const Book = (props) => {
 // const Author2= () => {
 
 
-// return <h4> Jordan Moore </h4>;
+// return <h4> James Clear </h4>;
 // };
   
 
