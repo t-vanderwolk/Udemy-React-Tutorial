8198550bc5c8620d8982f1061838a52215f91f81
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-03 10:47:30 -0700

    key prop

--- src/index.js
+++ src/index.js
@@ -6,19 +6,24 @@ const books = [
 {
   author: "Ron DeSantis",
   title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
-  img: './images/book-1.jpg'
+  img: './images/book-1.jpg',
+  id: 1
+ 
 },
 
 {
   author: "James Clear",
   title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-  img: './images/book-2.jpg'
+  img: './images/book-2.jpg',
+  id: 2,
 },
 
 {
   author: "Deborah Hopkinson",
   title: "My Little Golden Book About Betty White",
-  img: './images/book-3.jpg'
+  img: './images/book-3.jpg',
+  id: 3,
+  
 },
 ];
 
@@ -32,9 +37,10 @@ const BookList =() => {
   return (
   <section className="booklist">
     {books.map((book) => {
-     const {img, title, author} = book
+     const {img, title, author, id} = book
       return (
-        <Book img={img} title={title} author={author} />
+        <Book img={img} title={title} author={author} 
+        key={id}/>
       )
     
     })}
