daad332d13875b925def7731ac6e59b5f34fb214
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-03 17:19:23 -0700

    event basics

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ const BookList =() => {
 
   return (
   <section className="booklist">
+    <EventExamples />
     {books.map((book) => {
      
       return <Book {...book} key={book.id}/>;
@@ -38,6 +39,32 @@ const BookList =() => {
   </section>
   );
 };
+
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
+
+
 const Book = (props) => {
   const { img, title, author } = props;
 console.log(props)
