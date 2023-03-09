5e79679b0e13f646fec49dce2ee466d41f4ed5ab
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-09 09:27:46 -0700

    ES6 Modules

---
+++ src/books.js
@@ -0,0 +1,25 @@
+export const books = [
+    {
+      author: "Ron DeSantis",
+      title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
+      img: './images/book-1.jpg',
+      id: 1
+     
+    },
+    
+    {
+      author: "James Clear",
+      title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      img: './images/book-2.jpg',
+      id: 2,
+    },
+    
+    {
+      author: "Deborah Hopkinson",
+      title: "My Little Golden Book About Betty White",
+      img: './images/book-3.jpg',
+      id: 3,
+      
+    },
+    ];
+   
--- src/index.js
+++ src/index.js
@@ -1,65 +1,29 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 import './index.css';
+import {books} from './books'
+import Book from "./Book";
 
-const books = [
-{
-  author: "Ron DeSantis",
-  title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
-  img: './images/book-1.jpg',
-  id: 1
- 
-},
 
-{
-  author: "James Clear",
-  title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-  img: './images/book-2.jpg',
-  id: 2,
-},
-
-{
-  author: "Deborah Hopkinson",
-  title: "My Little Golden Book About Betty White",
-  img: './images/book-3.jpg',
-  id: 3,
-  
-},
-];
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
+  // const getBook = (id) => {
+  //   const book = books.find((book) => book.id === id);
+  //   console.log(book);
+  // };
 
 return (
   <section className='booklist'>
+   
     {books.map((book) => {
-      return <Book {...book} key={book.id} getBook={getBook} />;
+      return <Book {...book} key={book.id} />;
     })}
   </section>
 );
 };
 
 
-const Book = (props) => {
-  const { img, title, author, getBook, id} = props;
-   
-  const getSingleBook = () => {
-    getBook(id)
-  }
-  
-    
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>click me</button>
-      <h4>{author} </h4>
-    </article>
-  );
-};
+
 
 
 
